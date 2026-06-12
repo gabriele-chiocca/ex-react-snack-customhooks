dcc80e71194932189a5a318b1880462ce2562f7f
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-06-12 11:42:05 +0200

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import useSwitch from './hooks/useSwitch';
+import useDate from './hooks/useDate';
 
 function App() {
-  const [isOn, toggle] = useSwitch();
+  // const [isOn, toggle] = useSwitch();
+  // return (
+  //   <div>
+  //     <h1>Il valore è: {isOn ? 'ON' : 'OFF'}</h1>
+  //     <button onClick={toggle}>Cambia Stato</button>
+  //   </div>
+  // );
+
+  const currentDate = useDate();
 
   return (
     <div>
-      <h1>Il valore è: {isOn ? 'ON' : 'OFF'}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
+      <h1>Data e ora attuali:</h1>
+      <p>{currentDate.toLocaleString()}</p>
     </div>
   );
 }
